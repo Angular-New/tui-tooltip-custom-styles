e781321b8f3f33d63d164ae4cefe0beff8dd67aa
Verda Muso <gearmobile@gmail.com>
2023-12-07 18:11:40 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'other-attempt';
+  public title: string = 'other-attempt';
 }
