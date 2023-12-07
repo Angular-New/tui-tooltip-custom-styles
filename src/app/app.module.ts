744cca5fd415b882de9b1f884bee4b92d8c2f45b
Verda Muso <gearmobile@gmail.com>
2023-12-07 20:42:39 +0300

    try tui-hint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import {TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER, TuiTooltipModule} from "@taiga-ui/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+  TUI_SANITIZER,
+  TuiAlertModule,
+  TuiDialogModule,
+  TuiHintModule,
+  TuiRootModule,
+  TuiTooltipModule,
+} from '@taiga-ui/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SgmTooltipComponent } from './components/sgm-tooltip/sgm-tooltip.component';
+import { TuiAvatarModule } from '@taiga-ui/kit';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,9 @@ import { SgmTooltipComponent } from './components/sgm-tooltip/sgm-tooltip.compon
     TuiRootModule,
     TuiDialogModule,
     TuiAlertModule,
-    TuiTooltipModule
+    TuiTooltipModule,
+    TuiHintModule,
+    TuiAvatarModule,
   ],
   providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
